74d4b7642a2a7061ff01e82e998fdcdf9cd6e9cd
myop6 <riargon@gmail.com>
2025-07-07 21:06:31 +0200

    Update tratarHorasCompleto1.py

--- tratarHorasCompleto1.py
+++ tratarHorasCompleto1.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import config
 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from openpyxl import load_workbook
+from openpyxl.styles import Font
+from openpyxl.formatting.rule import FormulaRule
+
+from seleccionar_ruta import seleccionar_ruta
+from tratarHoras import tratarHoras
+from tratar_certificaciones import tratar_certificaciones
+
+def tratarHorasCompleto1:
+
+  
