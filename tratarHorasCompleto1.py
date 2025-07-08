7f65ecd2a9483888475bf41ea5dcf12cff297651
myop6 <riargon@gmail.com>
2025-07-08 08:41:02 +0200

    Update tratarHorasCompleto1.py

--- tratarHorasCompleto1.py
+++ tratarHorasCompleto1.py
@@ -11,12 +11,12 @@ from seleccionar_ruta import seleccionar_ruta
 from tratarHoras import tratarHoras
 from tratar_certificaciones import tratar_certificaciones
 
-def tratarHorasCompleto1():
+def tratarHorasCompleto1(fichero, horasMesAsc,horasEscaleras,horasMesEsc, certificacion,personal,opcion_seleccionada):
 
   variable1=1;
-  ascensores=tratarHorasCompleto()
+  ascensores=tratarHorasCompleto(fichero, horasMesAsc,certificacion,personal,opcion_seleccionada)
   variable=2
-  escaleras=tratarHorasEscaleras()
+  escaleras=tratarHorasEscaleras(HorasEscaleras,HorasMesEsc,certificacion,personal,opcion_seleccionada)
 
 
   
