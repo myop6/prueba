378ca46aafc06810cbaced215d1b356a0d5464b1
myop6 <riargon@gmail.com>
2025-07-08 11:52:45 +0200

    Update tratarHorasEscaleras.py

--- tratarHorasEscaleras.py
+++ tratarHorasEscaleras.py
@@ -100,7 +100,7 @@ def tratarHorasEscaleras(fichero,fichero1, fichero3, opcion_seleccionada):
 
     final_completo = pd.merge(final, superPafiltrado, on="id", how="left")
 
-    columnas_ordenadas=["id","nombre completo","job title","job name","horas_mantenimiento_12meses","horas_cbk_12meses","horas_mantenimiento_mes","horas_cbk_mes","manager user sys id","supervisor","do","dr (dirección regional)","sucursal"]
+    columnas_ordenadas=["id","nombre completo","job title","job name","horas_mantenimiento_12meses","horas_cbk_12meses","horas_mantenimiento_mes","horas_cbk_mes","estado_certificacion","manager user sys id","supervisor","do","dr (dirección regional)","sucursal"]
     final_completo=final_completo[columnas_ordenadas]
 
     if config.variable1==2:
