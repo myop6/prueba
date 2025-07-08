2cd05dbe984cda2c0ff3900aa758340a3eb59cee
myop6 <riargon@gmail.com>
2025-07-08 14:38:25 +0200

    Update contar.py

--- contar.py
+++ contar.py
@@ -1,9 +1,10 @@
 import time
 
 def contar(segundos):
-    for i in range(1,segundos+1)
+    for i in range(1,segundos+1):
       print(f"segundo {i})
       time.sleep(1)
+   
     print("Contador terminado")
 
   
