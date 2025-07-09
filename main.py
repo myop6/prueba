f0b91753c4f7923897cbf9e2a40d33f484cc3b50
myop6 <riargon@gmail.com>
2025-07-09 10:48:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main():
             completo_rep(archivo_excel,archivo_rep)
         elif opcion_seleccionada == '7':
             break
-        elif opcion_seleccionada =='0':
+        elif opcion_seleccionada ==':':
             cantidad=int(input("Seleccione cantidad"))
             contar(cantidad)
         else:
