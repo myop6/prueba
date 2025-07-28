f2fcb3721833c81f0c0911ac31ddd17079ae7cef
myop6 <riargon@gmail.com>
2025-07-28 09:20:39 +0200

    Add files via upload

--- buscar_palabras.py
+++ buscar_palabras.py
@@ -5,11 +5,7 @@ def buscar_palabras(texto):
                         'atauva','não  atuaria','adaptar','modernizar','substituir rolamento','limitador de velocidade',
                         'deslocar puerta','mover porta','modificar porta','mudar o limitador','trocar limitador', 'substituir lv',
                         'shuntar','instalar vf', 'trocar regulador','suprimir','adaptar fechadura','adaptar variador',
-                        'trocar limitador', 'trocar vf',anular', 'sustituir limitador', 'sustituir maniobra', 'casquillo', 'encasquillar', 'anulado',
-                      'no actúa', 'actuaría', 'actuaba','no actuaría', 'adaptar', 'modernizar','sustituir rodamiento',
-                       'mover puerta','cambiar puerta','cambiar lv', 'cambiar limitador'
-                      'sustituir lv','puentear','puntear','punteado','puenteado','instalar VF','instalar var','cambiar regulador','suprimir',
-                      'adaptar cerr', 'adaptar variador','macizo','sustituir regulador','modificar','actua','actúa','no funciona','falta rodiillo','rodillo']
+                        'trocar limitador', 'trocar vf']
 
 
     if any(palabra in texto.lower() for palabra in palabras_clave):
